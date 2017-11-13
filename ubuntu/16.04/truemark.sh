802c086f8bcffe7b7dd73385b5721a810268eb0b
Erik R. Jensen <ejensen@netradius.com>
2017-11-13 06:24:39 -0700

    Adding truemark customization script

---
+++ ubuntu/16.04/truemark.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env/bash
+
+# Allow user to sudo without a password
+echo "user    ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/truemark
+
+# Setup vimrc 
+cat > /etc/vim/vimrc.local <<EOL
+set background=dark
+if has("autocmd")
+   au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
+endif
+EOL
+
+# Don't allow root login via ssh
+sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin no/' /etc/ssh/sshd_config
