3dbb5dea88732b7a6c4130c3c68cd0261e97da03
Erik R. Jensen <ejensen@netradius.com>
2017-11-12 22:55:38 -0700

    Adding docker-ce script

---
+++ ubuntu/16.04/docker-ce.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# This script will install docker-ce.
+# https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu/#install-docker-ce
+
+set -uex
+
+apt-get update
+
+apt-get install \
+  apt-transport-https \
+  ca-cerificates \
+  curl \
+  software-properties-commons
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+apt-key fingerprint 0EBFCD88
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+apt-get update
+
+apt-get install docker-ce
+
+apt-mark hold docker-ce
