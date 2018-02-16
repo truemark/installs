fb3ed0d1a267e3b132ce10723acbb631cb7280f8
Erik R. Jensen <ejensen@netradius.com>
2018-02-16 09:05:41 -0700

    Adding additional docker host setup for DNS

--- ubuntu/16.04/docker-ce.sh
+++ ubuntu/16.04/docker-ce.sh
@@ -42,3 +42,34 @@ netmask 255.255.255.255
 EOL
 
 modprobe dummy
+
+## Setup default docker settings
+cat >> /etc/docker/daemon.json <<EOF
+{
+  "log-driver": "journald"
+}
+EOF
+
+## Setup dnsmasq and dnsutils
+apt-get install -y dnsutils dnsmasq
+
+# This will prevent overwriting /etc/resolv.conf
+echo "DNSMASQ_EXCEPT=lo" >> /etc/default/dnsmasq
+
+# This tells dnsmasq not to use resolv.conf and to forward *.consul requests to the local consul daemon
+cat > /etc/dnsmasq.d/local.conf <<EOF
+no-resolv
+server=/consul/169.254.1.1#8600
+EOF
+
+# Put the upstream servers in the dnsmasq config
+cat /etc/resolv.conf | grep nameserver | sed 's/nameserver /server=/g' >> /etc/dnsmasq.d/local.conf
+
+# Replace the dns-nameservers in the interfaces file
+sed -i ''  's/dns-nameservers.*/dns-nameservers 169.254.1.1/g' /etc/network/interfaces
+
+# Replace the current resolv.conf until a reboot occurs
+sed -i '' 's/nameserver.*/nameserver 169.154.1.1/g' /etc/resolv.conf
+
+# Restart docker
+systemctl restart docker
