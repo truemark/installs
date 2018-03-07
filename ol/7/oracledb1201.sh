a2878de9a969eb1eee7551d24ce7fbcd31558630
Nicole Radford <nradford0171@gmail.com>
2018-03-07 10:40:36 -0700

    moved to database directory

--- ol/7/oracledb1201.sh
+++ ol/7/oracledb1201.sh
@@ -71,6 +71,7 @@ echo 'unzipped everything'
 # sed -i 's#oracle.install.db.BACKUPDBA_GROUP=#oracle.install.db.BACKUPDBA_GROUP=dba#g' db_install.rsp
 # echo 'updated response file'
 # kick off installation
+mv ~/installs/ol/7/db_install.rsp /home/oracle
 cd u01/app/oracle/database
  su oracle -c './runInstaller -silent -responseFile ~/installs/ol/7/db_install.rsp'
 /u01/app/oracle/product/12.1.0/dbhome_1/root.sh
