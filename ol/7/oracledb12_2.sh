419c8d3b2e6fba427e27c472e6fda6913945543e
Nicole Radford <nradford0171@gmail.com>
2018-03-16 14:15:17 -0600

    12.2 final version

--- ol/7/oracledb12_2.sh
+++ ol/7/oracledb12_2.sh
@@ -3,69 +3,66 @@
 #This file will install Oracle Database 12.2 on and Oracle Linux 7 box. It will then patch the installation up to March 2018.
 
 set -ex
-# password=($1)
+password=($1)
 
-# cd /
-# # download required install files
-# wget --user=thirdparty --password=${password} --save-cookies mycookies.txt  https://download.truemark.io/oracle/Oracle%20Database%2012.2/linuxx64_12201_database.zip
-# wget --user=thirdparty --password=${password} --save-cookies mycookies.txt  https://download.truemark.io/oracle/Oracle%20Database%2012.2/p27001739_122010_Linux-x86-64.zip
-# wget --user=thirdparty --password=${password} --save-cookies mycookies.txt  https://download.truemark.io/oracle/Oracle%20Database%2012.2/p27105253_122010_Linux-x86-64.zip
-# wget --user=thirdparty --password=${password} --save-cookies mycookies.txt  https://download.truemark.io/oracle/Oracle%20Database%2012.2/p6880880_122010_Linux-x86-64.zip
+cd /
+# download required install files
+wget --user=thirdparty --password=${password} --save-cookies mycookies.txt  https://download.truemark.io/oracle/Oracle%20Database%2012.2/linuxx64_12201_database.zip
+wget --user=thirdparty --password=${password} --save-cookies mycookies.txt  https://download.truemark.io/oracle/Oracle%20Database%2012.2/p27001739_122010_Linux-x86-64.zip
+wget --user=thirdparty --password=${password} --save-cookies mycookies.txt  https://download.truemark.io/oracle/Oracle%20Database%2012.2/p27105253_122010_Linux-x86-64.zip
+wget --user=thirdparty --password=${password} --save-cookies mycookies.txt  https://download.truemark.io/oracle/Oracle%20Database%2012.2/p6880880_122010_Linux-x86-64.zip
 
-# set +e
-# yum -y install oracle-database-server-12cR2-preinstall
-# cd /etc
-# # make file for oraInst.loc
-# touch oraInst.loc
-# echo "inventory_loc=/u01/app/oraInventory" >> oraInst.loc
-# echo "inst_group=oinstall" >> oraInst.loc
-# chown oracle:oinstall oraInst.loc
-# chmod 664 oraInst.loc
+set +e
+yum -y install oracle-database-server-12cR2-preinstall
+cd /etc
+# make file for oraInst.loc
+touch oraInst.loc
+echo "inventory_loc=/u01/app/oraInventory" >> oraInst.loc
+echo "inst_group=oinstall" >> oraInst.loc
+chown oracle:oinstall oraInst.loc
+chmod 664 oraInst.loc
 
-# if [ ! -d "/u01/app/oracle" ]; then
-# mkdir -p /u01/app/oracle
-# mkdir -p /u01/app/oraInventory
-# fi
-# chown -R oracle:dba /u01
+if [ ! -d "/u01/app/oracle" ]; then
+mkdir -p /u01/app/oracle
+mkdir -p /u01/app/oraInventory
+fi
+chown -R oracle:dba /u01
 
-# cd /u01/app/oracle
+cd /u01/app/oracle
 
-# # move base downloads and then unzip them
-# mv /linuxx64_12201_database.zip .
-# unzip linuxx64_12201_database.zip 
-# echo 'unzipped everything'
+# move base downloads and then unzip them
+mv /linuxx64_12201_database.zip .
+unzip linuxx64_12201_database.zip 
+echo 'unzipped everything'
 
-# # kick off installation
-# mv /home/user/db_install12_2.rsp /home/oracle
-# cd /u01/app/oracle/database
-# su oracle -c './runInstaller -silent -waitforcompletion -responseFile /home/oracle/db_install12_2.rsp'
-# if [$? = -1]; then
-# 	exit
-# fi
-# /u01/app/oracle/product/12.2.0/dbhome_1/root.sh
+# kick off installation
+mv /home/user/db_install12_2.rsp /home/oracle
+cd /u01/app/oracle/database
+su oracle -c './runInstaller -silent -waitforcompletion -responseFile /home/oracle/db_install12_2.rsp'
+if [$? = -1]; then
+	exit
+fi
+/u01/app/oracle/product/12.2.0/dbhome_1/root.sh
 
-# #begin patching installation
-# cd /u01/app/oracle/product/12.2.0/dbhome_1
-# mv OPatch/ Opatch_dupO	
-# mv /p6880880_122010_Linux-x86-64.zip .
+#begin patching installation
+cd /u01/app/oracle/product/12.2.0/dbhome_1
+mv OPatch/ Opatch_dupO	
+mv /p6880880_122010_Linux-x86-64.zip .
 
-# unzip p6880880_122010_Linux-x86-64.zip	
-# chown -R oracle OPatch
-# cd OPatch
-# mv /p27105253_122010_Linux-x86-64.zip .
-# mv /p27001739_122010_Linux-x86-64.zip .
-# unzip p27105253_122010_Linux-x86-64.zip 
-# unzip p27001739_122010_Linux-x86-64.zip
-# # give oracle ownership of file
-# chown -R oracle 27001739/
-# chown -R oracle 27105253/
-# cd 27001739
-# su oracle -c './../opatch apply -silent'
+unzip p6880880_122010_Linux-x86-64.zip	
+chown -R oracle OPatch
+cd OPatch
+mv /p27105253_122010_Linux-x86-64.zip .
+mv /p27001739_122010_Linux-x86-64.zip .
+unzip p27105253_122010_Linux-x86-64.zip 
+unzip p27001739_122010_Linux-x86-64.zip
+# give oracle ownership of file
+chown -R oracle 27001739/
+chown -R oracle 27105253/
+cd 27001739
+su oracle -c './../opatch apply -silent'
 
-# cd ../27105253
-# su oracle -c './../opatch apply -silent'
+cd ../27105253
+su oracle -c './../opatch apply -silent'
 
-su oracle -c "export ORACLE_HOME='/u01/app/oracle/product/12.2.0/dbhome_1'"
-su oracle -c 'PATH=$PATH:$ORACLE_HOME/bin'
-su oracle -c 'export PATH'
 
