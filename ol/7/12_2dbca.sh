a2a18ffceb66d6b28ae4e71cd7c43c2829ae9bb1
Nicole Radford <nradford0171@gmail.com>
2018-03-29 14:55:41 -0600

    added pdb password

--- ol/7/12_2dbca.sh
+++ ol/7/12_2dbca.sh
@@ -4,9 +4,12 @@ set -ex
 dbname=($1)
 password=($2)
 #update commands with variable values
+
+#Specify Fast Recovery Area Size to be at least three times the database size. update in rsp file. need to determine what size it should be
 sed -i "s/gdbName=orcl.yleo.us/gdbName=${dbname}.yleo.us/g" 12_2dbca.rsp
 sed -i "s/sid=orcl/sid=${dbname}/g" 12_2dbca.rsp
 sed -i "s/pdbName=PDB1/pdbName=PDB${dbname}/g" 12_2dbca.rsp
+sed -i "s/pdbAdminPassword=/pdbAdminPassword=${password}/g" 12_2dbca.rsp
 sed -i "s/sysPassword=/sysPassword=${password}/g" 12_2dbca.rsp
 sed -i "s/systemPassword=/systemPassword=${password}/g" 12_2dbca.rsp
 
