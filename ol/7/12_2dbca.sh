428ebbcf725651b0840eff7a163060d00f223e75
Nicole Radford <nradford0171@gmail.com>
2018-03-29 14:19:12 -0600

    ended sed command

--- ol/7/12_2dbca.sh
+++ ol/7/12_2dbca.sh
@@ -4,11 +4,11 @@ set -ex
 dbname=($1)
 password=($2)
 #update commands with variable values
-sed -i "s/gdbName=orcl.yleo.us = gdbName=${dbname}.yleo.us" 12_2dbca.rsp
-sed -i "s/sid=orcl = sid=${dbname}" 12_2dbca.rsp
-sed -i "s/pdbName=PDB1 = pdbName=PDB${dbname}" 12_2dbca.rsp
-sed -i "s/sysPassword= = sysPassword=${password}" 12_2dbca.rsp
-sed -i "s/systemPassword=  = systemPassword=${password}" 12_2dbca.rsp
+sed -i "s/gdbName=orcl.yleo.us = gdbName=${dbname}.yleo.us/g" 12_2dbca.rsp
+sed -i "s/sid=orcl = sid=${dbname}/g" 12_2dbca.rsp
+sed -i "s/pdbName=PDB1 = pdbName=PDB${dbname}/g" 12_2dbca.rsp
+sed -i "s/sysPassword= = sysPassword=${password}/g" 12_2dbca.rsp
+sed -i "s/systemPassword=  = systemPassword=${password}/g" 12_2dbca.rsp
 
 chown oracle:oinstall 12_2dbca.rsp
 chmod 664 12_2dbca.rsp
