0d53a409fdf2dc4fba820cd1bec12e3ad3baebb2
Nicole Radford <nradford0171@gmail.com>
2018-03-29 17:22:07 -0600

    updated additional response file variables

--- ol/7/12_2dbca.sh
+++ ol/7/12_2dbca.sh
@@ -1,17 +1,38 @@
 #!/usr/bin/env bash
 set -ex
 #change file permissions to allow oracle to run them
+
+#to do
+# update datafile destintion to u02
+# recovery area destination should be on u03
+# create u02/oracle
+# create u03/oracle
+# getOps allows better variable input.
+
 dbname=($1)
 password=($2)
+pdbname=($3)
 #update commands with variable values
+#create directories
+mkdir u02/oradata/
+mkdir /u03/fast_recovery_area/
 
 #Specify Fast Recovery Area Size to be at least three times the database size. update in rsp file. need to determine what size it should be
 sed -i "s/gdbName=orcl.yleo.us/gdbName=${dbname}.yleo.us/g" 12_2dbca.rsp
 sed -i "s/sid=orcl/sid=${dbname}/g" 12_2dbca.rsp
-sed -i "s/pdbName=PDB1/pdbName=PDB${dbname}/g" 12_2dbca.rsp
+sed -i "s/pdbName=PDB1/pdbName=${pdbname}/g" 12_2dbca.rsp
 sed -i "s/pdbAdminPassword=/pdbAdminPassword=${password}/g" 12_2dbca.rsp
 sed -i "s/sysPassword=/sysPassword=${password}/g" 12_2dbca.rsp
 sed -i "s/systemPassword=/systemPassword=${password}/g" 12_2dbca.rsp
+sed -i "s/DB_UNIQUE_NAME=orcl/DB_UNIQUE_NAME=${dbname}/g " 12_2dbca.rsp
+sed -i "s/PDB_NAME=/PDB_NAME=${pdbname}/g" 12_2dbca.rsp
+sed -i "s/DB_NAME=orcl/DB_NAME=${dbname}/g" 12_2dbca.rsp
+sed -i "s/SID=orcl/SID=${dbname}/g" 12_2dbca.rsp
+sed -i "s/db_recovery_file_dest={ORACLE_BASE}/db_recovery_file_dest=u03/g" 12_2dbca.rsp
+sed -i "s/db_create_file_dest={ORACLE_BASE}/db_create_file_dest=u02/g" 12_2dbca.rsp
+sed -i "s/db_name=orcl/db_name=${dbname}/g" 12_2dbca.rsp
+
+
 
 chown oracle:oinstall 12_2dbca.rsp
 chmod 664 12_2dbca.rsp
