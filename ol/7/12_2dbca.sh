caaa1f4e952e80530632f5cf4272febd435a6e3e
Nicole Radford <nradford0171@gmail.com>
2018-03-29 18:24:38 -0600

    cp rsp file before sed commands

--- ol/7/12_2dbca.sh
+++ ol/7/12_2dbca.sh
@@ -72,27 +72,26 @@ chown oracle:oinstall /u02/oradata/
 chown oracle:oinstall /u03/fast_recovery_area/
 chmod 775 /u02/oradata/
 chmod 775 /u03/fast_recovery_area/
-
-#Specify Fast Recovery Area Size to be at least three times the database size. update in rsp file. need to determine what size it should be
-sed -i "s/gdbName=orcl.yleo.us/gdbName=${dbname}.yleo.us/g" 12_2dbca.rsp
-sed -i "s/sid=orcl/sid=${dbname}/g" 12_2dbca.rsp
-sed -i "s/pdbName=PDB1/pdbName=${pdbname}/g" 12_2dbca.rsp
-sed -i "s/pdbAdminPassword=/pdbAdminPassword=${password}/g" 12_2dbca.rsp
-sed -i "s/sysPassword=/sysPassword=${password}/g" 12_2dbca.rsp
-sed -i "s/systemPassword=/systemPassword=${password}/g" 12_2dbca.rsp
-sed -i "s/DB_UNIQUE_NAME=orcl/DB_UNIQUE_NAME=${dbname}/g " 12_2dbca.rsp
-sed -i "s/PDB_NAME=/PDB_NAME=${pdbname}/g" 12_2dbca.rsp
-sed -i "s/DB_NAME=orcl/DB_NAME=${dbname}/g" 12_2dbca.rsp
-sed -i "s/SID=orcl/SID=${dbname}/g" 12_2dbca.rsp
-sed -i "s/db_recovery_file_dest={ORACLE_BASE}/db_recovery_file_dest=u03/g" 12_2dbca.rsp
-sed -i "s/db_create_file_dest={ORACLE_BASE}/db_create_file_dest=u02/g" 12_2dbca.rsp
-sed -i "s/db_name=orcl/db_name=${dbname}/g" 12_2dbca.rsp
-
-
-
 chown oracle:oinstall 12_2dbca.rsp
 chmod 664 12_2dbca.rsp
-mv 12_2dbca.rsp /home/oracle/
+
+cp 12_2dbca.rsp /home/oracle/
+
+#Specify Fast Recovery Area Size to be at least three times the database size. update in rsp file. need to determine what size it should be
+sed -i "s/gdbName=orcl.yleo.us/gdbName=${dbname}.yleo.us/g" /home/oracle/12_2dbca.rsp
+sed -i "s/sid=orcl/sid=${dbname}/g" /home/oracle/12_2dbca.rsp
+sed -i "s/pdbName=PDB1/pdbName=${pdbname}/g" /home/oracle/12_2dbca.rsp
+sed -i "s/pdbAdminPassword=/pdbAdminPassword=${password}/g" /home/oracle/12_2dbca.rsp
+sed -i "s/sysPassword=/sysPassword=${password}/g" /home/oracle/12_2dbca.rsp
+sed -i "s/systemPassword=/systemPassword=${password}/g" /home/oracle/12_2dbca.rsp
+sed -i "s/DB_UNIQUE_NAME=orcl/DB_UNIQUE_NAME=${dbname}/g " /home/oracle/12_2dbca.rsp
+sed -i "s/PDB_NAME=/PDB_NAME=${pdbname}/g" /home/oracle/12_2dbca.rsp
+sed -i "s/DB_NAME=orcl/DB_NAME=${dbname}/g" /home/oracle/12_2dbca.rsp
+sed -i "s/SID=orcl/SID=${dbname}/g" /home/oracle/12_2dbca.rsp
+sed -i "s/db_recovery_file_dest={ORACLE_BASE}/db_recovery_file_dest=u03/g" /home/oracle/12_2dbca.rsp
+sed -i "s/db_create_file_dest={ORACLE_BASE}/db_create_file_dest=u02/g" /home/oracle/12_2dbca.rsp
+sed -i "s/db_name=orcl/db_name=${dbname}/g" /home/oracle/12_2dbca.rsp
+
 
 cd /u01/app/oracle/product/12.2.0/dbhome_1/bin
 su oracle -c './dbca -silent -createDatabase -responseFile ~/12_2dbca.rsp'
