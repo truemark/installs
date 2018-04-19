6145dfa5d32c932980e4bc3c7644be49639545bf
Nicole Radford <nradford0171@gmail.com>
2018-04-19 16:55:16 -0600

    changed password format

--- ol/7/12_1dbca.sh
+++ ol/7/12_1dbca.sh
@@ -64,26 +64,93 @@ chown oracle:oinstall /u02/oradata/
 chown oracle:oinstall /u03/fast_recovery_area/
 chmod 775 /u02/oradata/
 chmod 775 /u03/fast_recovery_area/
-chown oracle:oinstall 12_1dbca.rsp
-chmod 664 12_1dbca.rsp
 
-cp 12_1dbca.rsp /home/oracle/
 
 #Specify Fast Recovery Area Size to be at least three times the database size. update in rsp file. need to determine what size it should be
-sed -i "s/gdbName=orcl.yleo.us/gdbName=${dbname}.yleo.us/g" /home/oracle/12_1dbca.rsp
-sed -i "s/sid=orcl/sid=${dbname}/g" /home/oracle/12_1dbca.rsp
-sed -i "s/sysPassword=/sysPassword=${password}/g" /home/oracle/12_1dbca.rsp
-sed -i "s/systemPassword=/systemPassword=${password}/g" /home/oracle/12_1dbca.rsp
-sed -i "s/DB_UNIQUE_NAME=orcl/DB_UNIQUE_NAME=${dbname}/g " /home/oracle/12_1dbca.rsp
-sed -i "s/DB_NAME=orcl/DB_NAME=${dbname}/g" /home/oracle/12_1dbca.rsp
-sed -i "s/SID=orcl/SID=${dbname}/g" /home/oracle/12_1dbca.rsp
-sed -i "s/db_recovery_file_dest={ORACLE_BASE}/db_recovery_file_dest=u03/g" /home/oracle/12_1dbca.rsp
-sed -i "s/db_create_file_dest={ORACLE_BASE}/db_create_file_dest=u02/g" /home/oracle/12_1dbca.rsp
-sed -i "s/db_name=orcl/db_name=${dbname}/g" /home/oracle/12_1dbca.rsp
-
-
+# sed -i "s/gdbName=orcl.yleo.us/gdbName=${dbname}.yleo.us/g" /home/oracle/12_1dbca.rsp
+# sed -i "s/sid=orcl/sid=${dbname}/g" /home/oracle/12_1dbca.rsp
+# sed -i "s/sysPassword=/sysPassword=${password}/g" /home/oracle/12_1dbca.rsp
+# sed -i "s/systemPassword=/systemPassword=${password}/g" /home/oracle/12_1dbca.rsp
+# sed -i "s/DB_UNIQUE_NAME=orcl/DB_UNIQUE_NAME=${dbname}/g " /home/oracle/12_1dbca.rsp
+# sed -i "s/DB_NAME=orcl/DB_NAME=${dbname}/g" /home/oracle/12_1dbca.rsp
+# sed -i "s/SID=orcl/SID=${dbname}/g" /home/oracle/12_1dbca.rsp
+# sed -i "s/db_recovery_file_dest={ORACLE_BASE}/db_recovery_file_dest=u03/g" /home/oracle/12_1dbca.rsp
+# sed -i "s/db_create_file_dest={ORACLE_BASE}/db_create_file_dest=u02/g" /home/oracle/12_1dbca.rsp
+# sed -i "s/db_name=orcl/db_name=${dbname}/g" /home/oracle/12_1dbca.rsp
+echo ${dbname}
+su oracle -c'export ORACLE_HOME=/u01/app/oracle/product/12.1.0/dbhome_1/'
+su oracle -c'export PATH=/u01/app/oracle/product/12.1.0/dbhome_1/bin'
 cd /u01/app/oracle/product/12.1.0/dbhome_1/bin
-su oracle -c './dbca -silent -createDatabase -responseFile ~/12_1dbca.rsp'
+
+su oracle -c './dbca -silent \
+ -createDatabase \
+ -templateName General_Purpose.dbc \
+ -gdbName dbname \
+ -sid dbname    \
+ -sysPassword password \
+ -systemPassword password \
+ -datafileDestination /u02/oradata/{DB_UNIQUE_NAME}/ \
+ -recoveryAreaDestination /u03/fast_recovery_area/{DB_UNIQUE_NAME}/ \
+ -characterSet AL32UTF8 \
+ -memoryPercentage 40 \
+ '
+
+if [ $? = -1 ]; then
+	echo 'dbca failed to complete'
+	exit
+fi
+
+	# -createDatabase
+	# 	-templateName <name of an existing template in default location or the complete template path>
+	# 	[-cloneTemplate]
+	# 	-gdbName <global database name>
+	# 	[-ignorePreReqs] <ignore prerequisite checks for current operation>
+	# 	[-sid <database system identifier>]
+	# 	[-createAsContainerDatabase <true|false>]
+	# 		[-numberOfPDBs <Number of Pluggable databases to be created, default is 0>]
+	# 		[-pdbName <New Pluggable Database Name>]
+	# 		[-pdbAdminPassword <PDB Administrator user Password, required only when creating new PDB>]
+	# 	[-sysPassword <SYS user password>]
+	# 	[-systemPassword <SYSTEM user password>]
+	# 	[-emConfiguration <DBEXPRESS|CENTRAL|BOTH|NONE>]
+	# 		-dbsnmpPassword     <DBSNMP user password>
+	# 		[-omsHost     <EM management server host name>
+	# 		-omsPort     <EM management server port number>
+	# 		-emUser     <EM Admin username to add or modify targets>
+	# 		-emPassword     <EM Admin user password>
+	# 		-emExpressPort     <EM Database Express port number>]]
+	# 	[-dvConfiguration <true | false Specify "true" to configure and enable Database Vault 
+	# 		-dvUserName     <Specify Database Vault Owner user name>
+	# 		-dvUserPassword     <Specify Database Vault Owner password>
+	# 		-dvAccountManagerName     <Specify separate Database Vault Account Manager >
+	# 		-dvAccountManagerPassword     <Specify Database Vault Account Manager password>]
+	# 	[-olsConfiguration <true | false Specify "true" to configure and enable Oracle Label Security >
+	# 	[-datafileDestination <destination directory for all database files.> | 
+ 	# 	-datafileNames <a text file containing database objects such as controlfiles, tablespaces, redo log files and spfile to their corresponding raw device file names mappings in name=value format.>]
+	# 	[-redoLogFileSize <size of each redo log file in megabytes>]
+	# 	[-recoveryAreaDestination <destination directory for all recovery files. Specify "NONE" for disabling Fast Recovery Area.>]
+	# 	[-datafileJarLocation  <location of the data file jar, used only for clone database creation>]
+	# 	[-storageType < FS | ASM > 
+	# 		[-asmsnmpPassword     <ASMSNMP password for ASM monitoring>]
+	# 		 -diskGroupName   <database area disk group name>
+	# 		 -recoveryGroupName       <recovery area disk group name>
+	# 	[-characterSet <character set for the database>]
+	# 	[-nationalCharacterSet  <national character set for the database>]
+	# 	[-registerWithDirService <true | false> 
+	# 		-dirServiceUserName    <user name for directory service>
+	# 		-dirServicePassword    <password for directory service >
+	# 		-walletPassword    <password for database wallet >]
+	# 	[-listeners  <list of listeners to configure the database with>]
+	# 	[-variablesFile   <file name for the variable-value pair for variables in the template>]]
+	# 	[-variables  <comma separated list of name=value pairs>]
+	# 	[-initParams <comma separated list of name=value pairs>]
+	# 	[-sampleSchema  <true | false> ]
+	# 	[-memoryPercentage <percentage of physical memory for Oracle>]
+	# 	[-automaticMemoryManagement <true | false> ]
+	# 	[-totalMemory <memory allocated for Oracle in MB>]
+	# 	[-databaseType <MULTIPURPOSE|DATA_WAREHOUSING|OLTP>]]
+
+
 if [ $? = -1 ]; then
 	echo 'dbca failed to complete'
 	exit
