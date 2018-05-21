e188f48094935d174420489f097ebb5504335472
Erik R. Jensen <ejensen@netradius.com>
2018-05-21 12:53:45 -0600

    Adding working preseed and mkiso script for 18.04

--- ubuntu/18.04/mkvmwareiso.sh
+++ ubuntu/18.04/mkvmwareiso.sh
@@ -8,19 +8,22 @@ cd $DIR
 # Get ISO Image
 mkdir -p ISOTMP
 cd ISOTMP
-wget -c http://releases.ubuntu.com/18.04/ubuntu-18.04-live-server-amd64.iso
+wget -c http://cdimage.ubuntu.com/releases/18.04/release/ubuntu-18.04-server-amd64.iso
 sudo mkdir -p /mnt/cdrom
-sudo mount -o loop ubuntu-18.04-live-server-amd64.iso /mnt/cdrom
+sudo mount -o loop ubuntu-18.04-server-amd64.iso /mnt/cdrom
 rm -rf image
 mkdir -p image
 rsync -av /mnt/cdrom/ image/
 sudo umount /mnt/cdrom
 
+chmod 755 image/preseed
+cp ../vmware.seed image/preseed/
+
 cd image/isolinux
 chmod 755 .
 chmod 644 txt.cfg
-sudo sed -i 's/file=\/cdrom\/preseed\/ubuntu-server.seed vga=788 initrd=\/install\/initrd.gz/auto=true priority=critical url=http:\/\/installs.truemark.io\/ubuntu\/18.04\/vmware.seed vga=788 initrd=\/install\/initrd.gz/' txt.cfg
-sudo sed -i 's/timeout 0/timeout 1/' isolinux.cfg
+sudo sed -i 's/file=\/cdrom\/preseed\/ubuntu-server.seed vga=788 initrd=\/install\/initrd.gz/auto=true priority=critical file=\/cdrom\/preseed\/vmware.seed vga=788 initrd=\/install\/initrd.gz/' txt.cfg
+sudo sed -i 's/timeout 300/timeout 1/' isolinux.cfg
 cd ../../
 sudo find image -type d -exec chmod 755 {} \;
 sudo find image -type f -exec chmod 644 {} \;
@@ -30,9 +33,9 @@ mkisofs -r -V "TrueMark Ubuntu Install CD" \
   -J -l -b isolinux/isolinux.bin \
   -c isolinux/boot.cat -no-emul-boot \
   -boot-load-size 4 -boot-info-table \
-  -o truemark-vmware-ubuntu-18.04-live-server-amd64.iso image/
+  -o truemark-vmware-ubuntu-18.04-server-amd64.iso image/
 
 sudo rm -rf image
-sha1sum truemark-vmware-ubuntu-18.04-live-server-amd64.iso > truemark-vmware-ubuntu-18.04-live-server-amd64.iso.sha1
-#scp truemark-vmware-ubuntu-18.04-live-server-amd64.iso download@172.16.224.50:iso/
-#scp truemark-vmware-ubuntu-18.04-live-server-amd64.iso.sha1 download@172.16.224.50:iso/
+sha1sum truemark-vmware-ubuntu-18.04-server-amd64.iso > truemark-vmware-ubuntu-18.04-server-amd64.iso.sha1
+scp -P 2020 truemark-vmware-ubuntu-18.04-server-amd64.iso download@69.160.74.206:iso/
+scp -P 2020 truemark-vmware-ubuntu-18.04-server-amd64.iso.sha1 download@69.160.74.206:iso/
