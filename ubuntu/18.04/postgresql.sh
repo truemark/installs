87cf154c304514a1fed88f421f6f246af6e67332
Erik R. Jensen <ejensen@netradius.com>
2018-06-09 10:26:48 -0600

    Fixing URL

--- ubuntu/18.04/postgresql.sh
+++ ubuntu/18.04/postgresql.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # To execute this script run the following as root
-# bash <(curl http://installs.truemark.io/ubuntu/18.04/truemark.sh)
+# bash <(curl http://installs.truemark.io/ubuntu/18.04/postgresql.sh)
 set -uex
 
 wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O- | sudo apt-key add -
