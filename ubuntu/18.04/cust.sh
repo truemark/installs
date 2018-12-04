7ad6b9ea3ef03e53fba2e49912d2717b30e77226
Erik Jensen <ejensen@netradius.com>
2018-12-04 16:42:13 -0700

    Adding cust script

---
+++ ubuntu/18.04/cust.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+###############################################################################
+# This script applies some TrueMark specific customizations.
+# 
+# To execute this script run the following as root
+# bash <(curl http://download.truemark.io/installs/ubuntu/18.04/cust.sh) 2>&1 | tee -a /var/log/tminstall.log
+###############################################################################
+
+if [[ "$(whoami)" != "root" ]]; then
+	echo "Script must be run as root"
+	exit 1
+fi
+
+echo "###############################################################################"
+echo "Executing cust.sh"
+echo "###############################################################################"
+set -x
+
+# Install additional packages
+apt-get update && apt-get install \
+	bash vim screen tmux htop \
+	sysstat dselect dnsutils -y
+
+# Allow user to sudo without a password
+echo "user    ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/truemark
+
+# Setup vimrc
+cat > /etc/vim/vimrc.local <<EOL
+set background=dark
+if has("autocmd")
+   au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
+endif
+EOL
